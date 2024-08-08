82054d44069a825e18a4cf9160bf4b3cde11052d
mbarcenas <misael.barcenas@yahoo.com>
2024-08-08 18:07:59 -0500

    array sum

---
+++ src/main/java/algorithms/SimpleArraySum.java
@@ -0,0 +1,42 @@
+package algorithms;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleArraySum {
+
+    public static int simpleArraySum(List<Integer> ar) {
+        // Write your code here
+        int sum = 0;
+        for(Integer num: ar){
+            sum += num;
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
