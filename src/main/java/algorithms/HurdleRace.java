9cb1e752ced22991eac81d4d7d0c35781431eac6
mbarcenas <misael.barcenas@yahoo.com>
2024-09-16 15:24:06 -0500

    finding whether or not a player can jump over a hurdle

---
+++ src/main/java/algorithms/HurdleRace.java
@@ -0,0 +1,48 @@
+package algorithms;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class HurdleRace {
+
+    public static int hurdleRace(int k, List<Integer> height) {
+        // Write your code here
+        Collections.sort(height);
+        int maxHeight = height.get(height.size()-1);
+
+        if(maxHeight - k >= 0){
+            return maxHeight - k;
+        }
+        return 0;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> height = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = hurdleRace(k, height);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+
+}
