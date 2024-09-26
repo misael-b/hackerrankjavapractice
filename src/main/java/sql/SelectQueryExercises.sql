34eb4f0aa3bd72a05cbeaa3771315d2b4a5b9772
mbarcenas <misael.barcenas@yahoo.com>
2024-09-26 10:39:08 -0500

    substrings in SQL

--- src/main/java/sql/SelectQueryExercises.sql
+++ src/main/java/sql/SelectQueryExercises.sql
@@ -16,3 +16,32 @@ SELECT
 (SELECT COUNT(DISTINCT CITY) FROM STATION);
 --use SELECT () - () to subtract two counts
 -- use COUNT() to count number  of columns
+
+
+SELECT CITY, LENGTH(CITY) FROM STATION
+ORDER BY length(CITY), CITY ASC LIMIT 1;
+
+SELECT CITY, LENGTH(CITY) FROM STATION
+ORDER BY length(CITY) DESC, CITY ASC LIMIT 1;
+
+--use length() function to find length of string
+--use LIMIT to select number of entries
+
+
+SELECT DISTINCT CITY FROM STATION
+WHERE SUBSTRING(CITY, 1,1) = "A" OR
+SUBSTRING(CITY, 1,1) = "E" OR
+SUBSTRING(CITY, 1,1) = "I" OR
+SUBSTRING(CITY, 1,1) = "O" OR
+SUBSTRING(CITY, 1,1) = "U";
+
+--substring to select subsections of string, starts at 1;
+-- substring(string, start, numberOfCharacters)
+
+SELECT DISTINCT CITY FROM STATION
+WHERE SUBSTRING(CITY, -1, 1) = "A" OR
+SUBSTRING(CITY, -1, 1) = "E" OR
+SUBSTRING(CITY, -1, 1) = "I" OR
+SUBSTRING(CITY, -1, 1) = "O" OR
+SUBSTRING(CITY, -1, 1) = "U";
+--substring : last letter (-1)
