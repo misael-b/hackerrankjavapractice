540c9dffaf1dbbeaf5dc10056320525ad60c197f
mbarcenas <misael.barcenas@yahoo.com>
2024-10-10 18:00:56 -0500

    two queries

--- src/main/java/sql/SelectQueryExercises.sql
+++ src/main/java/sql/SelectQueryExercises.sql
@@ -252,3 +252,8 @@ SELECT CEIL((AVG(Salary)) - AVG(REPLACE(Salary,0,""))) FROM EMPLOYEES;
 SELECT MAX(salary*months), COUNT(employee_id) FROM Employee WHERE salary*months = (SELECT MAX(salary*months) FROM EMPLOYEE);
 -- find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings
 
+SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2) FROM STATION;
+-- Query the following two values from the STATION table:
+--    1. The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+--    2. The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+
