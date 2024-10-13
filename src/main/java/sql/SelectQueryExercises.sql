77a59b3a18c6444d1a9d687c8c9b34b9c555a454
mbarcenas <misael.barcenas@yahoo.com>
2024-10-13 18:22:15 -0500

    using two funtions

--- src/main/java/sql/SelectQueryExercises.sql
+++ src/main/java/sql/SelectQueryExercises.sql
@@ -263,3 +263,7 @@ SELECT TRUNCATE(SUM(LAT_N),4) FROM STATION WHERE LAT_N BETWEEN 38.7880 AND 137.2
 -- greater than 38.7880 and less than 137.2345
 -- Truncate your answer to decimal places.
 -- Could use BETWEEN
+
+SELECT TRUNCATE(MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
+-- Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345
+-- Truncate your answer to 4 decimal places
