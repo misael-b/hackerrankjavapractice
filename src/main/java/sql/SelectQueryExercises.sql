451ad02f0da2c7b6d69706bf63198e8613d6a99a
mbarcenas <misael.barcenas@yahoo.com>
2024-10-13 18:26:01 -0500

    query longitude by selecting max latitude

--- src/main/java/sql/SelectQueryExercises.sql
+++ src/main/java/sql/SelectQueryExercises.sql
@@ -267,3 +267,7 @@ SELECT TRUNCATE(SUM(LAT_N),4) FROM STATION WHERE LAT_N BETWEEN 38.7880 AND 137.2
 SELECT TRUNCATE(MAX(LAT_N), 4) FROM STATION WHERE LAT_N < 137.2345;
 -- Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345
 -- Truncate your answer to 4 decimal places
+
+SELECT ROUND(LONG_W, 4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than 137.2345
+-- Round your answer to 4 decimal places.
