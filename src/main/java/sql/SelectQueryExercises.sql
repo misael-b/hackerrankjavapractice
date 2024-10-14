82ad5879013391f64bd801a46aac6862e28e01b1
mbarcenas <misael.barcenas@yahoo.com>
2024-10-14 15:30:06 -0500

    query longitude from min latitude

--- src/main/java/sql/SelectQueryExercises.sql
+++ src/main/java/sql/SelectQueryExercises.sql
@@ -275,3 +275,7 @@ SELECT ROUND(LONG_W, 4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATI
 SELECT ROUND(MIN(LAT_N), 4) FROM STATION WHERE LAT_N > 38.7780;
 -- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780
 -- Round your answer to 4 decimal places.
+
+SELECT ROUND(LONG_W, 4) FROM STATION WHERE LAT_N = (SELECT MIN(LAT_N) FROM STATION WHERE LAT_N > 38.7780);
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than 38.7780
+-- Round your answer to 4 decimal places.
